efb7f3793d108ed2438268795bda665a56e999a4
Sankaran Srinivasan <sankaran.srinivasan@Sankarans-MacBook-Pro-2.local>
2023-09-12 10:43:02 +0530

    Half written test cases

---
+++ src/main/java/org/tw/bootcamp/Value.java
@@ -0,0 +1,15 @@
+package org.tw.bootcamp;
+
+public class Value {
+    public Value(int i) {
+
+    }
+
+    public boolean same(Value value2) {
+        return false;
+    }
+
+    public boolean equals(Value value2) {
+        return false;
+    }
+}
